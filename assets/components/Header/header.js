7deb9d9034f22bb1d8a2a38e40c823f66ab44a52
Bianca Bobotis <ext_biancaro@mercadolivre.com>
2021-11-09 15:05:57 -0300

    Alterações do CSS do Header

---
+++ assets/components/Header/header.js
@@ -0,0 +1,44 @@
+const header = function () {
+  document.body.innerHTML = `
+    <header id="header">
+    <section class="header-logo">
+        <a href="../MainPage/MainPage.html"> <img class="algoritmo-logo"
+                src="../../assets/img/algoritmo horizontal.png"></a>
+    </section>
+
+    <section class="search-bar">
+        <input type="text" class="search-bar-input"
+    placeholder="Buscar produtos, sabores e muito mais..."><button class="search-btn"><img class= "search-logo" 
+        src="../../assets/img/search-solid.svg"></button> 
+    </section>
+
+    <section class="btn-container-itens">
+        <button class="menu-item-car" type="button" id="menu-item-car" data-tooltip="Carrinho"><img class= "shopping-cart-logo" 
+            src="../../assets/img/shopping-cart-solid.svg"></button>
+        <button class="menu-item-orders" type="button" id="menu-item-orders" data-tooltip="Meus pedidos"><img class="clipboard-logo" 
+            src="../../assets/img/clipboard-list-solid.svg"></button>
+        <button class="menu-item-profile" type="button" id="menu-item-profile" data-tooltip="Meu perfil" ><img class="user-logo" 
+            src="../../assets/img/user-solid.svg"></button>
+        <a class="menu-item-exit" id="menu-item-exit">Sair</a>
+    </section>
+</header>
+</body>
+${document.body.innerHTML}
+`
+}
+header();
+
+const profileRedirect = document.getElementById('menu-item-profile');
+profileRedirect.addEventListener('click', myProfileRedirect);
+
+function myProfileRedirect() {
+  location.href = "../UserProfilePage/UserProfilePage.html";
+}
+
+const exit = document.getElementById('menu-item-exit');
+exit.addEventListener('click', exitAndClean);
+
+function exitAndClean() {
+  sessionStorage.clear();
+  location.href = "../LoginPage/LoginPage.html";
+}
