dd7f5c02120a2b9da9c99c093e07089d181d6a6f
stevelyn <ext_stevelyn@mercadolivre.com>
2021-11-11 12:05:43 -0300

    Alteração no User model

--- models/users.models.js
+++ models/users.models.js
@@ -1,11 +1,20 @@
 import idUser from "../utils/UserId.util.js" ;
 
 class User {
-  constructor(nickname, password) {
+  constructor(nickname, password, name = '', address = {}, phone = '', updateAt = '', createdAt = new Date(Date.now()).toISOString()) {
     this.id = idUser().toString();
     this.nickname = nickname.toLowerCase();
     this.password = btoa(password);
-    this.createdAt = new Date(Date.now()).toISOString();
+    this.name = name || '' ;
+    this.address = {
+      description:  address.description || '',
+      number:  address.number  || '',
+      landmark:  address.landmark  || '',
+      complement:  address.complement  || '',
+    }
+    this.phone = phone;
+    this.createdAt = createdAt;
+    this.updateAt = updateAt;
   }
 };
 
