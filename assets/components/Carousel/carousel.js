d5ae6c1bd3907dbe1c7ec489db2487fbd6a903fa
stevelyn <ext_stevelyn@mercadolivre.com>
2021-12-10 12:10:39 -0300

    Correção dos erros

--- assets/components/Carousel/carousel.js
+++ assets/components/Carousel/carousel.js
@@ -1,8 +1,8 @@
 function getCarouselContainer(element) {
-	let carouselContainer = '';
+  let carouselContainer = '';
 
-	for (var i = 0; i < element.length; i++) {
-		carouselContainer = `
+  for (var i = 0; i < element.length; i++) {
+    carouselContainer = `
 			${carouselContainer}
 			<div class="carousel-container fade${i === 0 ? '' : ' hide'}">
     	<img src=${element[i].image} style="width: 45%; height: 100%;">
@@ -18,42 +18,42 @@ function getCarouselContainer(element) {
         <button class='pizza-buy'><img src="../../assets/img/shopping-cart-solid.svg"></button>
 		</div>
     </div>`
-	}
+  }
 
-	return carouselContainer;
+  return carouselContainer;
 }
 
 function addCarouselEvent() {
-	let slideIndex = 1;
-	const slides = document.getElementsByClassName('carousel-container');
+  let slideIndex = 1;
+  const slides = document.getElementsByClassName('carousel-container');
 
-	const prev = document.querySelector('.previous-arrow');
-	prev.addEventListener('click', () => moveSlides(-1));
+  const prev = document.querySelector('.previous-arrow');
+  prev.addEventListener('click', () => moveSlides(-1));
 
-	const next = document.querySelector('.next-arrow');
-	next.addEventListener('click', () => moveSlides(1));
+  const next = document.querySelector('.next-arrow');
+  next.addEventListener('click', () => moveSlides(1));
 
-	function moveSlides(number) {
-		showSlides(slideIndex += number);
-	}
+  function moveSlides(number) {
+    showSlides(slideIndex += number);
+  }
 
-	function showSlides(number) {
-		if (number > slides.length) {
-			slideIndex = 1;
-		}
-		if (number < 1) {
-			slideIndex = slides.length
-		}
-		for (var i = 0; i < slides.length; i++) {
-			slides[i].classList.add('hide');
-		}
-		slides[slideIndex - 1].classList.remove('hide');
-	}
+  function showSlides(number) {
+    if (number > slides.length) {
+      slideIndex = 1;
+    }
+    if (number < 1) {
+      slideIndex = slides.length
+    }
+    for (var i = 0; i < slides.length; i++) {
+      slides[i].classList.add('hide');
+    }
+    slides[slideIndex - 1].classList.remove('hide');
+  }
 }
 
 function Carousel(element, query) {
-	query.innerHTML =
-		`<section class="carousel">
+  query.innerHTML =
+    `<section class="carousel">
 			<div class='div-previous-arrow'>
 				<a class="previous-arrow">&#10094;</a>
 			</div>
@@ -63,7 +63,7 @@ function Carousel(element, query) {
 			</div>
 		</section>`
 
-	addCarouselEvent();
+  addCarouselEvent();
 }
 
 export default Carousel;
